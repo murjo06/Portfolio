4e34a348a1cffb0b470d7e2127a12fd2435b4b77
murjo06 <98845896+murjo06@users.noreply.github.com>
2023-06-05 23:09:59 +0200

    Commit on day 2

--- main.js
+++ main.js
@@ -1,64 +1,83 @@
 import * as THREE from "three";
 import { Sky } from "./sky.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { Body } from "./physics.js";
+import { Vector3, Clock } from "three";
 
+// boilerplate code
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.addEventListener("change", () => {renderer.render(scene, camera)});
 controls.enableZoom = false;
 controls.enablePan = false;
+const ambientLight = new THREE.AmbientLight(0x404040, 5);
+scene.add(ambientLight);
 
-let geometry = new THREE.BoxGeometry( 1, 1, 1 );
-let material = new THREE.MeshBasicMaterial({color: 0x444444});
-let cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+
+const gravity = 0;
+const thrust = 50;
+const terminalVelocity = 10;
+const rotationSpeed = 0.1;
+const resistance = 0.05;
+
+
+let plane = new THREE.Object3D();
+let planeBody;
+
+function initPlane() {
+    const loader = new GLTFLoader();
+    loader.load("models/plane/scene.gltf", function(gltf) {
+    	scene.add(gltf.scene);
+        plane = gltf.scene;
+    }, undefined, function(error) {
+    	console.error(error);
+    });
+}
+initPlane();
+planeBody = new Body(plane, 10);
+planeBody.applyForce(new Vector3(0, gravity * planeBody.mass, 0));
+
 
 function initSky() {
-    let sky = new Sky();
+    const sky = new Sky();
     sky.scale.setScalar(450000);
     scene.add(sky);
-
-    let sun = new THREE.Vector3();
-
+    const sun = new THREE.Vector3();
+    const sunLight = new THREE.PointLight(0xf2f2f2, 5);
+    scene.add(sunLight);
     const effectController = {
         turbidity: 10,
         rayleigh: 3,
         mieCoefficient: 0.005,
         mieDirectionalG: 0.7,
         elevation: 2,
-        azimuth: 180,
-        exposure: renderer.toneMappingExposure
+        azimuth: 180
     };
-    function guiChanged() {
-        const uniforms = sky.material.uniforms;
-        uniforms["turbidity"].value = effectController.turbidity;
-        uniforms["rayleigh"].value = effectController.rayleigh;
-        uniforms["mieCoefficient"].value = effectController.mieCoefficient;
-        uniforms["mieDirectionalG"].value = effectController.mieDirectionalG;
-
-        const phi = THREE.MathUtils.degToRad(90 - effectController.elevation);
-        const theta = THREE.MathUtils.degToRad(effectController.azimuth);
-
-        sun.setFromSphericalCoords(1, phi, theta);
-
-        uniforms["sunPosition"].value.copy(sun);
-
-        renderer.toneMappingExposure = effectController.exposure;
-        renderer.render(scene, camera);
-    }
-    guiChanged();
+    const uniforms = sky.material.uniforms;
+    uniforms["turbidity"].value = effectController.turbidity;
+    uniforms["rayleigh"].value = effectController.rayleigh;
+    uniforms["mieCoefficient"].value = effectController.mieCoefficient;
+    uniforms["mieDirectionalG"].value = effectController.mieDirectionalG;
+    const phi = THREE.MathUtils.degToRad(90 - effectController.elevation);
+    const theta = THREE.MathUtils.degToRad(effectController.azimuth);
+    sun.setFromSphericalCoords(1, phi, theta);
+    sunLight.position.x = sun.x;
+    sunLight.position.y = sun.y;
+    sunLight.position.z = sun.z;
+    uniforms["sunPosition"].value.copy(sun);
+    renderer.render(scene, camera);
 }
 initSky();
 
-camera.position.y = 5;
-camera.rotateX(Math.PI * 3 / 2);
-camera.rotateZ(Math.PI * 3 / 2);
+
+camera.position.y = 10;
+camera.rotateX(Math.PI * 1.5);
+camera.rotateZ(Math.PI * 1.5);
 
 const sqrt2 = Math.sqrt(2);
 
@@ -68,7 +87,6 @@ const movementControls = {
     "d": [0, 1],
     "a": [0, -1]
 };
-const speed = 0.1;
 let moving = false;
 
 let pressedKeys = [];
@@ -98,20 +116,74 @@ document.addEventListener("keyup", function(event) {
 
 });
 
+const clock = new Clock(true);
+
+function getThrust(multiplier) {
+    return new Vector3(
+        thrust * (Math.sin(plane.rotation._y) + Math.cos(plane.rotation._z)) * multiplier,
+        thrust * (Math.sin(plane.rotation._z) + Math.cos(plane.rotation._x) - 1) * multiplier,
+        thrust * (Math.sin(plane.rotation._x) + Math.cos(plane.rotation._y) - 1) * multiplier
+    );
+}
+function getResistance(multiplier, speed) {
+    return speed > 0.1 ? new Vector3(
+        -resistance * planeBody.velocityDirection.x * multiplier * speed * speed,
+        -resistance * planeBody.velocityDirection.y * multiplier * speed * speed,
+        -resistance * planeBody.velocityDirection.z * multiplier * speed * speed
+    ) : new Vector3(0, 0, 0);
+}
+
+let thrustForces = new Vector3(0, 0, 0);
+let dragForces = new Vector3(0, 0, 0);
+
 function animate() {
 	requestAnimationFrame(animate);
-    if(moving) {
-        let includesW = pressedKeys.includes("w") ? 1 : 0;
-        let includesS = pressedKeys.includes("s") ? -1 : 0;
-        let includesD = pressedKeys.includes("d") ? 1 : 0;
-        let includesA = pressedKeys.includes("a") ? -1 : 0;
-        let direction = [includesW + includesS, includesD + includesA];
-        if(direction[0] != 0 && direction[1] != 0) {
-            direction[0] /= sqrt2;
-            direction[1] /= sqrt2;
-        }
-        cube.position.x += direction[0] * speed;
-        cube.position.z += direction[1] * speed;
+    let includesW = pressedKeys.includes("w");
+    let includesS = pressedKeys.includes("s");
+    let includesD = pressedKeys.includes("d");
+    let includesA = pressedKeys.includes("a");
+    let includesSpace = pressedKeys.includes(" ");
+    let includesShift = pressedKeys.includes("Shift");
+    if(planeBody == undefined) {
+        planeBody = new Body(plane, 10, 10);
+    }
+    let oldDirection = new Vector3().copy(planeBody.velocityDirection);
+    let thrustForce = getThrust(includesSpace ? 1 : 0);
+    thrustForces.x += thrustForce.x;
+    thrustForces.y += thrustForce.y;
+    thrustForces.z += thrustForce.z;
+    planeBody.applyForce(thrustForce);
+    planeBody.speed = planeBody.velocity.length();
+    let drag = getResistance(includesShift ? 20 : 1, planeBody.speed);
+    planeBody.applyForce(drag);
+    dragForces.x += drag.x;
+    dragForces.y += drag.y;
+    dragForces.z += drag.z;
+    let delta = clock.getDelta();
+    planeBody.velocity.x += planeBody.acceleration.x * delta;
+    planeBody.velocity.y += planeBody.acceleration.y * delta;
+    planeBody.velocity.z += planeBody.acceleration.z * delta;
+    planeBody.velocityDirection.copy(planeBody.velocity).normalize();
+    planeBody.speed = planeBody.velocity.length();
+    if(planeBody.speed > terminalVelocity) {
+        let k = terminalVelocity / planeBody.speed;
+        planeBody.velocity.x = planeBody.velocity.x * k;
+        planeBody.velocity.y = planeBody.velocity.y * k;
+        planeBody.velocity.z = planeBody.velocity.z * k;
+    }
+    Math.round((planeBody.velocity.x + Number.EPSILON) * 100) / 100;
+    Math.round((planeBody.velocity.z + Number.EPSILON) * 100) / 100;
+    Math.round((planeBody.velocity.y + Number.EPSILON) * 100) / 100;
+    if(planeBody.velocity.x * oldDirection.x < 0 || planeBody.velocity.y * oldDirection.y < 0 || planeBody.velocity.z * oldDirection.z < 0) {
+        planeBody.removeForce(dragForces);
+        dragForces = new Vector3(0, 0, 0);
+    }
+    plane.position.x += planeBody.velocity.x * delta;
+    plane.position.y += planeBody.velocity.y * delta;
+    plane.position.z += planeBody.velocity.z * delta;
+    if(!includesSpace) {
+        planeBody.removeForce(thrustForces);
+        thrustForces = new Vector3(0, 0, 0);
     }
 	renderer.render(scene, camera);
 }
---
+++ physics.js
@@ -0,0 +1,22 @@
+import { Vector3 } from "three";
+
+class Body {
+    constructor(body, mass, terminalVelocity) {
+        this.body = body;
+        this.mass = mass;
+        this.forces = new Vector3(0, 0, 0);
+        this.acceleration = new Vector3(this.forces.x / mass, this.forces.y / mass, this.forces.z / mass);
+        this.velocity = new Vector3(0, 0, 0);
+        this.velocityDirection = new Vector3(0, 0, 0);
+        this.speed = 0;
+    }
+    applyForce(force) {
+        this.forces = new Vector3(this.forces.x + force.x, this.forces.y + force.y, this.forces.z + force.z);
+        this.acceleration = new Vector3(this.forces.x / this.mass, this.forces.y / this.mass, this.forces.z / this.mass);
+    }
+    removeForce(force) {
+        this.forces = new Vector3(this.forces.x - force.x, this.forces.y - force.y, this.forces.z - force.z);
+        this.acceleration = new Vector3(this.forces.x / this.mass, this.forces.y / this.mass, this.forces.z / this.mass);
+    }
+}
+export { Body }
